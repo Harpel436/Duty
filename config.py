17bb9134ac1c3df1cc666daf9bdbf24d180715db
Harpel436 <andriyb436@gmail.com>
2025-02-07 13:21:15 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+NAMES = ["Олександр", "Тест", "Ольга", "Іван", "Петро", "@harpel436", "Андрій @harpel436"]
+BOT_TOKEN = "7805025714:AAEO5Y0BiUnYM9ZR-iulaAemG5C-Kj44Adw"
+CHAT_ID = "-4715743102"
